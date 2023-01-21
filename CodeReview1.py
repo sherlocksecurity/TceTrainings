9ebc8f3e0af99da106c7d9c5ace43d5c22b5e568
Sherlock Secure <52328067+sherlocksecurity@users.noreply.github.com>
2023-01-21 16:24:48 +0530

    Create CodeReview1.py

---
+++ CodeReview1.py
@@ -0,0 +1,28 @@
+import os
+from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+
+@app.route('/user/<user_id>')
+def user(user_id):
+    user = User.query.get(user_id)
+    return '<h1>Hello, {}!</h1>'.format(user.name)
+
+@app.route('/add_user', methods=['POST'])
+def add_user():
+    name = request.form['name']
+    user = User(name=name)
+    db.session.add(user)
+    db.session.commit()
+    return 'Success'
+
+if __name__ == '__main__':
+    db.create_all()
+    app.run(debug=True)
